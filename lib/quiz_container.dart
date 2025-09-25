ddef6fb03a2aeb2978dede0d5eac80364704962b
Akhele <moneimb8@gmail.com>
2025-09-25 09:42:16 +0100

    Changing the opacity of the image

--- lib/quiz_container.dart
+++ lib/quiz_container.dart
@@ -21,14 +21,15 @@ class _QuizContainerState extends State<QuizContainer> {
               mainAxisSize: MainAxisSize.min,
               children: [
                 Image.asset("assets/images/quiz-logo.png",
-                  width: 200,),
+                  width: 200,
+                  color: const Color.fromARGB(153, 255, 255, 255),),
                 SizedBox(height: 20,),
                 Text('Quiz', style: TextStyle(color: Colors.white, fontSize: 28),),
                 OutlinedButton.icon(onPressed: (){}, 
                             style: OutlinedButton.styleFrom(foregroundColor: Colors.white,
                                     textStyle: TextStyle(fontSize: 28)
                             ),
-                            icon: Icon(Icons.assistant_photo_outlined),
+                            icon: Icon(Icons.assistant_photo_outlined,),
                             label: Text('Start'))
             ],
             );
